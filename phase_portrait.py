06316ac314fa1f0a77af704b4c24819477520788
Karpo <karpovehalex@gmail.com>
2022-02-26 00:30:37 +0300

    Обновлена функция рисования портрета. Добавлены примеры

--- phase_portrait.py
+++ phase_portrait.py
@@ -8,13 +8,12 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-def slice_data(df, slice_period, reset_index=True):
+def slice_data(df, slice_period=1, reset_index=True):
     # 2 сортировки, чтобы последняя дата оставалась
     if reset_index:
-        return df.copy().sort_values(by='date', ascending=False)[::slice_period].sort_values(by='date').reset_index(
-            drop=True)
+        return df.sort_values(by='date', ascending=False)[::slice_period].sort_values(by='date').reset_index()
     else:
-        return df.copy().sort_values(by='date', ascending=False)[::slice_period].sort_values(by='date')
+        return df.sort_values(by='date', ascending=False)[::slice_period].sort_values(by='date')
 
 
 def get_arange(length, step):
@@ -41,27 +40,34 @@ def calc_spline_values(xx, pp):
     return ppf
 
 
-def interpolate(ppf, step):
+def interpolate(ppf, step, params):
+    if params is None:
+        params = {}
+    kind = params.get('interpolate.kind', 'cubic')
+    fill_value = params.get('interpolate.fill_value', 'extrapolate')
     # Задаем интервалы для интерполяции
     t = np.arange(1, ppf[0].shape[0] + 1)
     pp1f_y = get_arange(ppf[0].shape[0], step=step)
     # Производим интерполяцию
-    interp1 = lambda x: interp1d(t, x, kind='cubic', fill_value="extrapolate")(pp1f_y)  # нужен ли параметр fill_value
+    interp1 = lambda x: interp1d(t, x, kind=kind, fill_value=fill_value)(pp1f_y)  # нужен ли параметр fill_value
     deriv = []
     for i in range(len(ppf)):
         deriv.append(interp1(ppf[i]))
     return deriv
 
 
-def get_phase_portrait(x, y, step_spline=0.05, step_deriv=.01, deriv_num=4):
+def get_phase_portrait(x, y, step_spline=0.05, step_deriv=.01, deriv_num=4, params=None):
+    if params is None:
+        params = {}
+    bc_type = params.get('get_phase_portrait.bc_type', 'not-a-knot')
     # Высчитываем данные портрета и xx
     xx = get_arange(x.shape[0], step=step_spline)
-    spline = CubicSpline(x, y, bc_type='not-a-knot')  # clamped
+    spline = CubicSpline(x, y, bc_type=bc_type)  # clamped
     # yy = spline(xx)
     pp = deriv_spline(spline, deriv_num)
     ppf = calc_spline_values(xx, pp)
 
-    deriv = interpolate(ppf, step_deriv)
+    deriv = interpolate(ppf, step_deriv, params)
     return xx, deriv
 
 
@@ -70,16 +76,19 @@ def get_ticks(arr, num=5):
     assert num > 2
 
     mask = np.linspace(0, arr.shape[0] - 1, num=num).round().astype(int)
-    if type(arr) == pd.core.frame.DataFrame or type(arr) == pd.core.series.Series:
+    if type(arr) == pd.DataFrame or type(arr) == pd.Series:
         return [arr.iloc[i] for i in mask]
     else:
         return [arr[i] for i in mask]
 
 
 def plot_phase_portrait(x, tt, deriv, dates, start_date='', end_date='', cbar_ticks_num=5, graph_name='', ylabel='Rate',
-                        xlabel='Value', cmap=mpl.cm.plasma):
+                        xlabel='Value', cmap=mpl.cm.plasma, fig=None, ax=None, plot_bar=True,
+                        background_graph_params=None, rcParams=None):
     '''
 
+    :param rcParams: Параметры для настройки графика в целом.
+    :param background_graph_params: Параметры для фонового графика.
     :param deriv: Производные сплайна.
     :param tt: Интерполированные значения x.
     :param dates: Даты.
@@ -92,12 +101,17 @@ def plot_phase_portrait(x, tt, deriv, dates, start_date='', end_date='', cbar_ti
     :param cmap: Цветовая схема для наложения на график.
     :return:
     '''
-    plt.rcParams.update({'xtick.labelsize': 15})
-    plt.rcParams.update({'ytick.labelsize': 15})
+    if not background_graph_params:
+        background_graph_params = {}
+    if not rcParams:
+        rcParams = {'xtick.labelsize': 15, 'ytick.labelsize': 15}
+    plt.rcParams.update(rcParams)
 
-    fig, ax = plt.subplots()
-    if not start_date and not end_date:
+    if fig is None or ax is None:
+        fig, ax = plt.subplots()
+    if not start_date:
         start_date = dates.min()
+    if not end_date:
         end_date = dates.max()
 
     ## Находим маску для выбранного периода
@@ -106,109 +120,68 @@ def plot_phase_portrait(x, tt, deriv, dates, start_date='', end_date='', cbar_ti
     tt_colored = tt[colored_mask]
 
     ## Рисуем фоновый график
-    ax.plot(deriv[0], deriv[1], ':g', alpha=0.7)
+    ax.plot(deriv[0], deriv[1], alpha=0.7, **background_graph_params)
 
     ### Красим выбранный период графика
     norm = mpl.colors.Normalize(vmin=tt_colored.min(), vmax=tt_colored.max())
-    ax.scatter(deriv[0][colored_mask], deriv[1][colored_mask], c=tt, cmap=cmap, s=0.1)
-    cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-                        orientation='vertical', ticks=get_ticks(tt_colored, cbar_ticks_num))
-    cbar.ax.set_yticklabels(map(lambda x: x.strftime('%d.%m.%y'), get_ticks(dates[dates_colored_mask], cbar_ticks_num)))
+    ax.scatter(deriv[0][colored_mask], deriv[1][colored_mask], c=tt_colored, cmap=cmap, s=0.1)
+    if plot_bar:
+        cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                            orientation='vertical', ticks=get_ticks(tt_colored, cbar_ticks_num))
+        cbar.ax.set_yticklabels(
+            map(lambda x: x.strftime('%d.%m.%y'), get_ticks(dates[dates_colored_mask], cbar_ticks_num)))
     ###
 
     if graph_name:
-        plt.title(graph_name)
+        ax.set_title(graph_name)
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel)
 
     # Выделение линии Y=0
-    plt.axhline(y=0, color='k', linestyle='-')
+    ax.axhline(y=0, color='k', linestyle='-')
 
     # настройка размера фигуры
-    fig.set_figheight(8)
-    fig.set_figwidth(9)
-    plt.show()
+    # fig.set_figheight(8)
+    # fig.set_figwidth(9)
+    return fig, ax
 
 
-def plot_phase_portrait_old(xx, dates, deriv, tt, name, ticks_num=5, save_fig_name='', y_limits=(), x_limits=()):
-    cmap = plt.cm.plasma
-    norm = plt.Normalize(xx.min(), xx.max())
-
-    c_bar = plt.cm.plasma(np.linspace(0, 1, tt.shape[0]))
-
-    fig, ax = plt.subplots()
-    ax.scatter(deriv[0], deriv[1], c=c_bar, s=0.05)
-    cax = ax.plot(deriv[0][-1], deriv[1][-1], '*g', markersize=6)
-    ax.annotate(dates.iloc[-1].strftime('%d.%m.%y'), xy=(deriv[0][-1], deriv[1][-1]), color='black', ha="right")
-    ax.set_ylabel('Rate')
-    ax.set_xlabel('Value')
-
-    ax.ticklabel_format(style='sci', scilimits=(-1, 4), axis='x')
-
-    plt.title(name)
-
-    fig.set_figheight(8)
-    fig.set_figwidth(8)
-
-    cbar = fig.colorbar(mpl.cm.ScalarMappable(cmap=cmap, norm=norm), ticks=get_ticks(xx, ticks_num),
-                        orientation='vertical')
-    cbar.ax.set_yticklabels(map(lambda x: x.strftime('%d.%m.%y'), get_ticks(dates, ticks_num)))
-    if len(y_limits) == 2:
-        plt.ylim(y_limits)
-    if len(x_limits) == 2:
-        plt.xlim(x_limits)
-    plt.grid()
-    if save_fig_name:
-        plt.savefig(f"{save_fig_name}.png", dpi=200)
-
-
-def get_filename(name, min_date, max_date, freq):
-    return f"{name}_{min_date}_to_{max_date}_f_{freq}"
-
-
-def calc_phase_portrait(df, slice_period=1, normalize=''):
-    if normalize:
-        normalization_value = df['value'][(df['date'] > pd.to_datetime(normalize, format='%d.%m.%Y'))].head(1).iloc[0]
-        df['value'] = df['value'] / normalization_value
-    else:
-        df['value'] = df['value'] / 1000
-    df = slice_data(df, slice_period)
+def calc_phase_portrait(df, slice_period=1, reset_index=False, params=None):
+    if params is None:
+        params = {}
+    df = slice_data(df, slice_period, reset_index)
 
     df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 
     x = df.index.to_series() + 1
     y = df['value']
-    x, y, xx, deriv, tt = calc_phase_portrait_raw(x, y)
+    x, y, xx, deriv, tt = calc_phase_portrait_raw(x, y, params=params)
 
     return x, y, xx, deriv, tt, slice_period, df['date']
 
 
-def calc_phase_portrait_raw(x, y):
-    xx, deriv = get_phase_portrait(x, y, step_spline=.05, step_deriv=.01, deriv_num=2)
-    tt = get_arange(x.shape[0], step=.0005)
+def calc_phase_portrait_raw(x, y, params=None):
+    if params is None:
+        params = {}
+    step_spline = params.get('calc_phase_portrait_raw.step_spline', .05)
+    step_deriv = params.get('calc_phase_portrait_raw.step_deriv', .01)
+    deriv_num = params.get('calc_phase_portrait_raw.deriv_num', 2)
+    tt_step = params.get('calc_phase_portrait_raw.tt_step', .0005)
+
+    xx, deriv = get_phase_portrait(x, y, step_spline=step_spline, step_deriv=step_deriv, deriv_num=deriv_num,
+                                   params=params)
+    tt = get_arange(x.shape[0], step=tt_step)
 
     return x, y, xx, deriv, tt
 
 
-def generate_phase_portrait(df, slices, normalize=''):
+def generate_phase_portrait(df, slices, params=None):
+    if params is None:
+        params = {}
     data = []
     for slice_period in slices:
-        data.append(calc_phase_portrait(df.copy(), slice_period, normalize))
+        data.append(calc_phase_portrait(df.copy(), slice_period, params=params))
     return data
-
-
-def generate_png(path, name, df, generated_data, x_lim, y_lim, upscale=1):
-    assert upscale > 0
-
-    for data in generated_data:
-        x, y, xx, deriv, tt, slice_period = data
-
-        filename = get_filename(name, df['date'].min().strftime('%d_%m_%y'), df['date'].max().strftime('%d_%m_%y'),
-                                slice_period)
-        deriv = list(map(lambda x: x * upscale, deriv))
-
-        plot_phase_portrait_old(xx, df['date'], deriv, tt, f"{name} t={slice_period}", ticks_num=5,
-                                save_fig_name=path + filename, y_limits=y_lim, x_limits=x_lim)
 
 
 def get_mask_tt(x, tt, dates=None, start_date=None, end_date=None, dates_mask=None):
